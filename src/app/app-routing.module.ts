9879353ff11c1379e9ec58141daf01aec77506ee
Arnaldo Neto <costanetoow@gmail.com>
2022-02-02 14:05:41 -0300

    Creating pokemons module and using routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'pokemons' },
+  {
+    path: 'pokemons',
+    loadChildren: () =>
+      import('./pokemons/pokemons.module').then((m) => m.PokemonsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
