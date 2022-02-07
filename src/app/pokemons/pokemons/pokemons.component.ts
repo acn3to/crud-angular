ba8421ad0eb317e308e89b3ce46cc0574522a03c
Arnaldo Neto <costanetoow@gmail.com>
2022-02-07 13:16:09 -0300

    Http get rxjs

--- src/app/pokemons/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons/pokemons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Pokemon } from '../model/pokemon';
 import { PokemonsService } from '../services/pokemons.service';
@@ -9,7 +10,7 @@ import { PokemonsService } from '../services/pokemons.service';
   styleUrls: ['./pokemons.component.scss'],
 })
 export class PokemonsComponent implements OnInit {
-  pokemons: Pokemon[] = [];
+  pokemons: Observable<Pokemon[]>;
   displayedColumns = [
     'name',
     'type1',
--- src/app/pokemons/services/pokemons.service.ts
+++ src/app/pokemons/services/pokemons.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pokemon } from '../model/pokemon';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonsService {
+  private readonly API = './assets/pokemons.json';
+
   constructor(private httpClient: HttpClient) {}
 
-  list(): Pokemon[] {
-    return [
-      {
-        _id: '1',
-        name: 'Bulbasaur',
-        type1: 'Grass',
-        type2: 'Poison',
-        healthPoint: 45,
-        attack: 49,
-        speed: 45,
-      },
-    ];
+  list() {
+    return this.httpClient
+      .get<Pokemon[]>(this.API)
+      .pipe(
+        first(),
+        tap((pokemons) => console.log(pokemons)));
   }
 }
